ba46c73968909addef3d462a07a14f82d8878e80
Diksha Jain <121768409+diksha2722@users.noreply.github.com>
2024-08-07 16:50:55 +0530

    Add files via upload

---
+++ Day1/variable.js
@@ -0,0 +1,38 @@
+var stringtype="Hello world";
+var Booleantype=true;
+var undefinedtype;
+var nulltype=null;
+
+//5. typeof
+
+console.log(typeof stringtype);
+console.log(typeof Booleantype);
+console.log(typeof undefinedtype);
+console.log(typeof nulltype);
+
+//6. Deaclare 4 variables without assigning values
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+
+//7.
+let var1 = "Hello";
+let var2 = 42;
+let var3 = true;
+let var4 = null;
+
+//8.
+let firstName = "John";
+let lastName = "Doe";
+let maritalStatus = "Single";
+let country = "USA";
+let age = 30;
+
+
+//10.
+let myAge=25;
+let yourAge=24;
+console.log("I am", myAge,"years old");
+console.log("You are", yourAge,"years old");
